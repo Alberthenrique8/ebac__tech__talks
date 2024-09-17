9fe48b439077842fe0abce643cb66ecd4bcdc79b
Albert Henrique <albertmine8@gmail.com>
2024-09-17 00:56:58 -0300

    criando a contagem regressiva

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -584,7 +584,30 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
-console.log("ola mundo");
+AOS.init();
+const dataDoEvento = new Date("Dec  12 , 2025  19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(()=>{
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaDoEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMS = 86400000;
+    const horaEmMs = 3600000;
+    const minutosEmMs = 60000;
+    const diasateoEvento = Math.floor(distanciaDoEvento / diaEmMS);
+    const horasAteoEvento = Math.floor(distanciaDoEvento % diaEmMS / horaEmMs);
+    const minutosAteoEvento = Math.floor(distanciaDoEvento % horaEmMs / minutosEmMs);
+    const segundosAteoEvento = Math.floor(distanciaDoEvento % minutosEmMs / 1000);
+    console.log(diasateoEvento);
+    console.log(horasAteoEvento);
+    console.log(minutosAteoEvento);
+    console.log(segundosAteoEvento);
+    document.getElementById("contador").innerHTML = `${diasateoEvento}d ${horasAteoEvento}h ${minutosAteoEvento}m ${segundosAteoEvento}s`;
+    if (distanciaDoEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento Expirado";
+    }
+}, 1000);
 
 },{}]},["8bDoD","3cYfC"], "3cYfC", "parcelRequireec61")
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('ola mundo');
+AOS.init();
+
+const dataDoEvento = new Date ("Dec  12 , 2025  19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(() => {
+    const agora = new Date ();
+    const timeStampAtual = agora.getTime()
+
+    const distanciaDoEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMS = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+
+
+
+    const diasateoEvento =Math.floor (distanciaDoEvento / diaEmMS)  ;
+    const horasAteoEvento =Math.floor ((distanciaDoEvento % diaEmMS) / horaEmMs);
+    const minutosAteoEvento = Math.floor((distanciaDoEvento % horaEmMs) / minutosEmMs);
+    const segundosAteoEvento = Math.floor((distanciaDoEvento % minutosEmMs) / 1000);
+
+    console.log(diasateoEvento);
+    console.log(horasAteoEvento);
+    console.log(minutosAteoEvento);
+    console.log(segundosAteoEvento);
+
+    document.getElementById('contador').innerHTML = `${diasateoEvento}d ${horasAteoEvento}h ${minutosAteoEvento}m ${segundosAteoEvento}s`;
+
+    if (distanciaDoEvento < 0 ) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado' 
+    }
+    
+}, 1000);
+
+
